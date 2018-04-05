e4b9b6df5905d5bb93834e95a70ee6444b8042de
Ismael Correa <Izzy@Ismaels-MacBook-Pro.local>
2018-04-05 12:59:09 -0500

    Psychic Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+var wins = 0;
+var losses = 0; 
+var GuessLeft = 10;
+var Guess = [,];
+
+function randomLetter() {
+    var Letter = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+  
+    for (var i = 0; i < 1; i++)
+      Letter += possible.charAt(Math.floor(Math.random() * possible.length));
+    return Letter;
+  }
+
+    document.onkeyup = function(event) {
+    var userguess = event.key;
+    var GuessSoFar = Guess + userguess;
+    Guess.fill(GuessSoFar);
+    function randomLetter() {
+      var Letter = "";
+      var possible = "abcdefghijklmnopqrstuvwxyz";
+    
+      for (var i = 0; i < 1; i++)
+        Letter += possible.charAt(Math.floor(Math.random() * possible.length));
+      return Letter;
+    }
+
+    if ( userguess === randomLetter()){
+      wins++;
+    }
+    else if (userguess != randomLetter()){
+      GuessLeft--;
+    }
+    if (GuessLeft === 0){
+      losses++;
+      GuessLeft = 9;
+      Guess = [,];
+    }
+
+    var html ="<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left " + GuessLeft + "</p>" +
+    "<p>Your guesses so far " + GuessSoFar + "</p>";
+  
+  document.querySelector("#game").innerHTML = html;
+
+}
+
